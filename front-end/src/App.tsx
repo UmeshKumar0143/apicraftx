5fc4f65c5de74ff9b2731484effaae7020bf1abf
Priyanshu Birkhani. <priyanshubirkhani229@gmail.com>
2025-04-23 00:51:41 +0530

    signin page

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -2,10 +2,14 @@ import DashBoard from "./components/DashBoard";
 import "./App.css"
 import MainPage from "./components/MainPage";
 import { Route, Routes } from "react-router-dom";
+import SignUp from "./components/SignUp";
+import SignIn from "./components/Signin";
 export default function App(){
   return <Routes>
   <Route path="/" element={<DashBoard />} />
   <Route path="/main" element={<MainPage />} />
+  <Route path="/signin" element={<SignIn />} />
+  <Route path="/signup" element={<SignUp />} />
 </Routes>
 
 }
