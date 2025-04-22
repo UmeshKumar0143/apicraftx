5a00a4d290779396009db3079b2f052015a9399d
UmeshKumar0143 <umeshkumar153654@gmail.com>
2025-04-23 04:58:47 +0530

    Schmea modify

--- backend/models/collection.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const CollectionSchema = new mongoose.Schema({
-  name: String,
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  baseUrl: String,
-  requests: Array
-});
-module.exports = mongoose.model('Collection', CollectionSchema);
